a37432f10e6dacb8fd5739275a6e46106535fb1d
manuonda <manuonda@gmail.com>
2024-06-28 00:26:06 -0300

    modificate flow welcome to verificate first exis user in database

--- src/flow/flowWelcome.ts
+++ src/flow/flowWelcome.ts
@@ -1,28 +1,42 @@
 import { EVENTS, addKeyword } from "@builderbot/bot";
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 import { flowMenu } from "./flowMenu";
-import { existUsuarioByTelefono } from "~/services/usuarioService";
+import { addUsuario, existUsuarioByTelefono, getUsuarioByTelephone } from "~/services/usuarioService";
 import { flowRegister } from "./flowRegister";
 
 
 
-export const flowWelcome = addKeyword<Provider>(['hola'])
-// .addAction(async (_, { flowDynamic }): Promise<void> => {
-//      console.log("informationc:");
-//      return flowDynamic('Hi! how can I help you?');
-//  })
- .addAction({ capture: true }, async (ctx, {provider, flowDynamic, state }): Promise<void> => {
+export const flowWelcome = addKeyword<Provider>(EVENTS.WELCOME)
+ .addAction({ capture: true }, async (ctx, {provider, flowDynamic, state, gotoFlow }): Promise<void> => {
      const checkNumber = ctx.from;
      const onWhats = await provider.vendor.onWhatsApp(checkNumber);
-     const existUsuario = await existUsuarioByTelefono(checkNumber);
+     const existUsuarioDB = await existUsuarioByTelefono(checkNumber);
     
-     if(!onWhats[0]?.exists){
+     if(!existUsuarioDB){
+         if(!onWhats[0]?.exists){
+             return gotoFlow(flowRegister);
+         } else {
+            await addUsuario({
+                first_name: ctx.name,
+                last_name: ctx.name,
+                telephone: ctx.from 
+            });
+         }
+     } else { 
+          // TODO if exist usuario in wsp puedo actualizar el usuario 
+          // en la base de datos
+          const usuario = await getUsuarioByTelephone(ctx.from);
+          await state.update({ name: usuario.first_name , telephone: ctx.from });   
+          return gotoFlow(flowMenu);
+     }
+     /*if(!onWhats[0]?.exists){
           await ctxFn.flowDynamic(`Not Exists: ${onWhats[0].exists}`)
      } else {
         //aqui le consulto para crear un registro al usuario   
         console.log("Existe el usuario");
         //verifico si existe en la base de datos para crearl
      } 
+        */
      console.log("information numero 2");
      await state.update({ name: ctx.body})
      return flowDynamic(`The user said: ${ctx.body}`);
