24696bfaa9f3b6b7eb79f5b93d6e79e8c594c777
david garcia <david.garcia@grupomost.com>
2024-07-11 23:56:33 -0300

    Agrego method para buscar el listado de recordatorios

--- src/services/reminderService.ts
+++ src/services/reminderService.ts
@@ -4,13 +4,6 @@ import { Reminder, Usuario } from "~/models/models";
 
 const table = "reminder";
 
-
-
-
-
-
-
-
 const addReminder = async (reminder: any, usuarioId: number) => {
   console.log("reminder : ", reminder);
   console.log("reminder.action :", reminder["Action"], reminder.Action)
@@ -33,19 +26,58 @@ const addReminder = async (reminder: any, usuarioId: number) => {
 }
 
 
-const convertToUsuarioData = (data: any[]): Usuario[] => {
+/**
+ * List Reminder by parameter
+ * @param idUsuario 
+ */
+const listReminder = async(idUsuario: number):Promise<Usuario[]> => {
+  try {
+    const { data, error } = await supabase
+      .from(table)
+      .select()
+      .eq('usuario_id', idUsuario);
+    if( data && data.length > 0 ){
+      return convertToData(data);
+    }
+    return [];
+  } catch (error) {
+    console.error(`Èrror guardar reminder :${error}`)
+     throw new Error(`Se produjo un error al obtener datos : ${error}`);
+  }
+}
+
+/**
+ * Update Reminder status
+ * @param idReminder 
+ * @param status 
+ * @returns 
+ */
+const updateReminderStatus= async ( idReminder: number , status: string): Promise<boolean> => {
+   try {
+    const { error } = await supabase
+    .from(table)
+    .update({ status})
+    .eq('id', idReminder)
+    if (!error) return true; return false;
+   } catch (error) {
+    console.error(`Error al actualizar el reminder: ${error}`)
+    return false;
+   }
+}
+
+const convertToData = (data: any[]): Reminder[] => {
   return data.map(item => ({
-    id: item.id,
-    name: item.name,
+    id:item.id,
+    title: item.title,
+    description: item.description,
+    reminder_date: item.reminder_date,
+    notification_time:  item.notification_time,
+    status: item.status,
+    usuario_id: item.usuario_id,
     created_at: item.created_at,
-    updated_at: item.updated_at,
-    first_name: item.first_name,
-    last_name: item.last_name,
-    email: item.email,
-    telephone: item.telephone
   }));
 }
 
 
 
-export {addReminder }
+export {addReminder , listReminder }
